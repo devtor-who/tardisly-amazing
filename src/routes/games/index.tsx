8b08cfba4b5ee04de0fec9309898c4f789198b61
Its-Dinner-Time <dinnertime.dev@gmail.com>
2024-03-03 04:03:22 +0900

    games page 스타일 일부 수정

--- src/routes/games/index.tsx
+++ src/routes/games/index.tsx
@@ -2,7 +2,7 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <section class={'h-dvh bg-neutral-700'}>
+    <section class={'h-dvh '}>
       <h1>Games Page</h1>
     </section>
   );
