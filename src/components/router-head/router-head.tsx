335a0b83256507cff05efa9c6057a4bd230f821f
Its-Dinner-Time <dinnertime.dev@gmail.com>
2024-03-03 17:45:51 +0900

    pwa 설정 수정

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -20,6 +20,10 @@ export const RouterHead = component$(() => {
         name="viewport"
         content="width=device-width, initial-scale=1.0"
       />
+      <meta
+        name="theme-color"
+        content="#3730a3"
+      />
       <link
         rel="icon"
         type="image/x-icon"
